d098a4d11256d34bd3ad23ef9b4d843cbf4dc4d0
Vitalii.Khomyk <vitalii.khomuk@gmail.com>
2022-08-14 16:02:07 +0300

    commit L2-T3

---
+++ lesson2/task3/src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles.css';
+
+const renderSeconds = time => {
+  const rootElement = document.querySelector('#root');
+
+  const seconds = new Date(time).getSeconds();
+  const backgroundColor = seconds % 2 === 0 ? '#fff' : '#000';
+  const textColor = seconds % 2 !== 0 ? '#fff' : '#000';
+
+  const styles = {
+    backgroundColor,
+    color: textColor,
+  };
+
+  const element = (
+    <div className="seconds" style={styles}>
+      Now is {seconds}
+    </div>
+  );
+
+  ReactDOM.render(element, rootElement);
+};
+renderSeconds(new Date());
+setInterval(() => renderSeconds(new Date()), 1000);
