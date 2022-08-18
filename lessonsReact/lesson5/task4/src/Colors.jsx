c99057cf4bed10e5e6af0dd6ab161bfd097a2704
Vitalii.Khomyk <vitalii.khomuk@gmail.com>
2022-08-19 00:40:15 +0300

    commit L5-T4

---
+++ lessonsReact/lesson5/task4/src/Colors.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+
+const RED = '#f00';
+const GREEN = '#0f0';
+const BLUE = '#00f';
+
+class Colors extends Component {
+  setBodyColor = color => {
+    document.body.style.backgroundColor = color;
+  };
+  render() {
+    return (
+      <div className="colors">
+        <button
+          style={{
+            backgroundColor: RED,
+          }}
+          onClick={() => this.setBodyColor(RED)}
+          // onClick={this.setBodyColor.bind(this, RED)}  // => 2sposib
+          className="colors__button"
+        />
+        <button
+          style={{
+            backgroundColor: GREEN,
+          }}
+          onClick={() => this.setBodyColor(GREEN)}
+          className="colors__button"
+        />
+        <button
+          style={{
+            backgroundColor: BLUE,
+          }}
+          onClick={() => this.setBodyColor(BLUE)}
+          className="colors__button"
+        />
+      </div>
+    );
+  }
+}
+
+export default Colors;
