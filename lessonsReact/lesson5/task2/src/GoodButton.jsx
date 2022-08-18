fb8c853bc54393def55079ae61779e74bf606256
Vitalii.Khomyk <vitalii.khomuk@gmail.com>
2022-08-18 20:41:36 +0300

    commit L5-T2

---
+++ lessonsReact/lesson5/task2/src/GoodButton.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class GoodButton extends React.Component {
+  handleClick(event) {
+    alert(event.target.textContent);
+  }
+  render() {
+    return (
+      <button className="fancy-button" onClick={this.handleClick}>
+        Click me!
+      </button>
+    );
+  }
+}
+
+export default GoodButton;
