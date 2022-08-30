a5bdbf5d4bbb7b42c66a45cee41b6c5b15c5856f
Vitalii.Khomyk <vitalii.khomuk@gmail.com>
2022-08-30 15:24:41 +0300

    commit L8-T1

---
+++ lessonsReact/lesson8/task1/src/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+// import Life from './Life.jsx';
+import Demo from './Demo';
+
+const rootElement = document.querySelector('#root');
+
+ReactDOM.render(<Demo />, rootElement);
