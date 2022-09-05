78c5dccd4e73d7cbd72c729a67e8d1b2f42d66b2
Vitalii.Khomyk <vitalii.khomuk@gmail.com>
2022-09-05 11:55:20 +0300

    commit l9-hw2

---
+++ lessonsReact/lesson9/hw2/src/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.scss';
+import Game from './Game';
+
+const rootElement = document.querySelector('#root');
+ReactDOM.render(<Game />, rootElement);
