d446b169807641861e450232d6647d068e61b49a
Vitalii.Khomyk <74536048+vitalii7845@users.noreply.github.com>
2022-09-12 21:05:13 +0300

    Delete lessonsReact/lesson12/todo-list directory

--- lessonsReact/lesson12/todo-list/babel.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  presets: [
-    ['@babel/preset-env', { targets: '> 1%', useBuiltIns: 'usage', corejs: 3 }],
-    '@babel/preset-react',
-  ],
-  plugins: ['@babel/plugin-proposal-class-properties'],
-};
