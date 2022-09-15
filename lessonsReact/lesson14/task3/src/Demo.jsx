732151aa1f2ae1a0787cecd7234f246807f8a8a3
Vitalii.Khomyk <vitalii.khomuk@gmail.com>
2022-09-16 02:58:12 +0300

    commit task3 - l14

---
+++ lessonsReact/lesson14/task3/src/Demo.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import Dimensions from './Dimensions';
+
+const Demo = () => {
+  const [isVisible, setIsVisible] = useState(true);
+
+  return (
+    <div>
+      <div>
+        <button className="btn" onClick={() => setIsVisible(true)}>
+          Show
+        </button>
+        <button className="btn" onClick={() => setIsVisible(false)}>
+          Hide
+        </button>
+      </div>
+      {isVisible && <Dimensions />}
+    </div>
+  );
+};
+export default Demo;
